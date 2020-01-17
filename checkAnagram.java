9a11d37a9471238f3bed27e2400a21b17fe6e300
SunnyGoyal319 <57326770+SunnyGoyal319@users.noreply.github.com>
2020-01-17 21:25:46 +0530

    Add files via upload

---
+++ checkAnagram.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class checkAnagram
+{
+	static boolean checkAnagram(String s,String s1)
+	{
+		int i;
+		int hash[]=new int[256];
+		int hash1[]=new int[256];
+		if(s.length()==s1.length())
+		{
+			for(i=0;i<s.length();i++)
+			{
+				hash[s.charAt(i)]++;
+			}
+			for(i=0;i<s1.length();i++)
+			{
+				hash1[s1.charAt(i)]++;
+			}
+			for(i=0;i<256;i++)
+			{
+				if(hash[i]!=hash1[i])
+				{
+					break;
+				}
+			}
+			if(i!=256)
+			{
+				return false;
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public static void main(String args[])
+	{
+		String s="test";
+		String s1="Tset";
+		if(checkAnagram(s,s1))
+		{
+			System.out.println("Anagram");
+		}
+
+		else
+		{
+			System.out.println("Not Anagram");
+		}
+	}
+}
